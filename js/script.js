fd00b19d8ee08beba7825eeddf4e2ee7d7210fc1
robherron <rlherron100@gmail.com>
2020-04-23 15:40:29 -0400

    Added Array to script

--- js/script.js
+++ js/script.js
@@ -11,7 +11,41 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-
+var quotes = [
+  {
+  quote: "I consider looseness with words no less of a defect than looseness of the bowels.",
+  source: "John Calvin"
+  },
+  {
+  quote: "God chooses us, not because we believe, but that we may believe.",
+  source: "Augustine of Hippo"
+  },
+  {
+  quote: "A believer's dying day is his ascension day to glory.",
+  source: "Thomas Watson"
+  },
+  {
+  quote: "If the final decision for the salvation of fallen sinners were left in the hands of fallen sinners, we would despair all hope that anyone would be saved.",
+  source: "R.C. Sproul"
+  },
+  {
+  quote: "It is evident, by both Scripture and reason, that God is infinitely, eternally, unchangeably, and independently glorious and happy: that He cannot be profited by, or receive any thing from, the creature.",
+  source: "Jonathan Edwards"
+  },
+  {
+  quote: "The love of God in our culture has been purged of anything the culture finds uncomfortable. The love of God has been sanitized, democratized, and above all sentimentalized." ,
+  source: "D.A. Carson"  
+  },
+  {
+  quote: "No matter how intense our experiences, no matter how acute our perceptions of what we think God would have us do, we cannot be certain God has spoken unless our revelation is based on Scripture." ,
+  source: "James Montgomery Boice"  
+  },
+  {
+  quote: "Rythyms of piety, soccer, Lord's Day Worship, Clemson, and Warm Greetings." ,
+  source: "John D Payne"
+  },
+]
+  console.log(quotes);
 
 /***
  * `getRandomQuote` function
