76c82a1fd976413987ec633e254af5418c6dfa7b
robherron <rlherron100@gmail.com>
2020-04-29 23:47:56 -0400

    Corrected a formatting issue and added comments

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * `quotes` array is an array of object literals consisting of a quote value, source value, and sometimes a citation and year value. 
 ***/
 
 var quotes = [
@@ -54,36 +54,40 @@ var quotes = [
 /***
  * `getRandomQuote` function returns a random quote from the quotes array
 ***/
+
+/***I struggled with understanding the purpose of parameters in a function, but with some help from a friend of mine I realized that the parameter is the information I put into
+ the function in order for the argument to pull from the array. Aka by making the parameter the array, I am telling the getRandomQuote function to return a random object from the 
+ array parameter, which is what getRandomQuote below does.
+*/
   function getRandomQuote (array) {
     return array[Math.floor(Math.random() * array.length)];
   }
   // stringQuote stylizes postedQuote (which is a pull from getRandomQuote) into the html
-  
-
-   
 /***
- * `printQuote` function
+ * `printQuote` function - this took the help of several folks in the Slack but I eventually figured out that for printQuote to work properly, I had to first declare a new variable
+ * that would be a concatination of the quote, source, etc. I already knew I needed an if else conditional in order to add the citations but had difficulty understanding the specific
+ * html instructions to give it. After some trial and error I realized that I had closed off the <p> prematurely and that was affecting format. Now printQuote will print the quote
+ * pulled from getRandomQuote into the "quote" class section within the "quote-box", the source into the "source" class, and where applicable, the citation and year in their respective
+ * span classes.
 ***/
 function printQuote () {
   var postedQuote= getRandomQuote (quotes);
-  var stringQuote = 
-    `<p class= "quote"> ${postedQuote.quote}</p>
-    <p class = "source"> ${postedQuote.source}</p>`;
+  var stringQuote =  
+    `<p class ="quote"> ${postedQuote.quote}</p>
+      <p class ="source"> ${postedQuote.source}`;
   // if the postedQuote has a citation or year value, this if else if conditional adds it to the html.
     if (postedQuote.citation) 
-     {stringQuote += `<p class "citation"<span class="citation"> <p>
-      ${postedQuote.citation}</span> </p>`;
+     {stringQuote += `<span class="citation">
+      ${postedQuote.citation}`;
     }
       else if (postedQuote.year)
-    {stringQuote += `<p class "source"><span class "year">     
+    {stringQuote += `<span class "year">  ,    
      ${postedQuote.year} </span> </p>`;};
   
   document.getElementById('quote-box').innerHTML = stringQuote; 
 };
 
 
-
-// getRandomQuote(quotes).quote, getRandomQuote(quotes).source
 
 /***
  * click event listener for the print quote button
