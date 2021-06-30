41fea8a723377fd305380fa435c2957186f64ace
robherron <rlherron100@gmail.com>
2021-06-30 14:24:35 -0400

    Cleanup

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array is an array of object literals consisting of a quote value, source value, and sometimes a citation and year value. 
-***/
 
 var quotes = [
   {
@@ -51,25 +40,13 @@ var quotes = [
 ];
   // console.log(quotes);
 
-/***
- * `getRandomQuote` function returns a random quote from the quotes array
-***/
 
-/***I struggled with understanding the purpose of parameters in a function, but with some help from a friend of mine I realized that the parameter is the information I put into
- the function in order for the argument to pull from the array. Aka by making the parameter the array, I am telling the getRandomQuote function to return a random object from the 
- array parameter, which is what getRandomQuote below does.
-*/
+
+
   function getRandomQuote (array) {
     return array[Math.floor(Math.random() * array.length)];
   }
-  // stringQuote stylizes postedQuote (which is a pull from getRandomQuote) into the html
-/***
- * `printQuote` function - this took the help of several folks in the Slack but I eventually figured out that for printQuote to work properly, I had to first declare a new variable
- * that would be a concatination of the quote, source, etc. I already knew I needed an if else conditional in order to add the citations but had difficulty understanding the specific
- * html instructions to give it. After some trial and error I realized that I had closed off the <p> prematurely and that was affecting format. Now printQuote will print the quote
- * pulled from getRandomQuote into the "quote" class section within the "quote-box", the source into the "source" class, and where applicable, the citation and year in their respective
- * span classes.
-***/
+
 function printQuote () {
   var postedQuote= getRandomQuote (quotes);
   var stringQuote =  
@@ -88,10 +65,5 @@ function printQuote () {
 };
 
 
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE!!
-***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
